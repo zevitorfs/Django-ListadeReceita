9d011694f69dcee596e37c5d92dccc6b1638959e
zetune910 <zetune910@gmail.com>
2024-05-27 17:26:13 -0300

    feat: :sparkles: Criação dos cards de receitas

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from recipes.views import home
+from recipes import views
 
 
 
 urlpatterns = [
     path('', home),
+    # no django so de por o <id> serve como parmetro para o recipes
+    path('recipes/<int:id>/', views.recipes),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 def home(request):
     #Função para localiza os documentos html
     return render(request, 'recipes/pages/home.html')
+
+#Não esqueça de por o paramentro aqui tambem
+def recipes(request, id):
+    #Função para localiza os documentos html
+    return render(request, 'recipes/pages/home.html')
